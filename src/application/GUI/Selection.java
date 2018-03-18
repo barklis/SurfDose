6c3fb176a73dc169dbac4b9e8e1299523b299ed4
xxlukasz11 <xxlukasz11@gmail.com>
2018-03-18 17:59:47 +0100

    inteface_update

--- src/application/GUI/Selection.java
+++ src/application/GUI/Selection.java
@@ -1,11 +1,110 @@
 package application.GUI;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public class Selection {
-	static List<Selection> selectionList;
+	static List<Selection> selectionList = new ArrayList<Selection>();
+	static List<Selection> deletedSelectionList = new ArrayList<Selection>();
+	static Selection currentSelection;
+	
+	double x1 = 0;
+	double x2 = 0;
+	double y1 = 0;
+	double y2 = 0;
+	
+	boolean exist = false;
 	
 	public Selection() {
+		currentSelection = null;
+	}
+	
+	public static void removeLast() {
+		if(selectionList.size() > 0) {
+			deletedSelectionList.add(selectionList.get(selectionList.size()-1));
+			selectionList.remove(selectionList.size()-1);
+		}
+	}
+	
+	public static void restoreSelection() {
+		if(deletedSelectionList.size() > 0) {
+			selectionList.add(deletedSelectionList.get(deletedSelectionList.size()-1));
+			deletedSelectionList.remove(deletedSelectionList.size()-1);
+		}
+	}
+	
+	
+	public boolean isExist() {
+		return exist;
+	}
+
+	public void setExist(boolean exist) {
+		this.exist = exist;
+	}
+	
+	public void drawOnCanvas(GraphicsContext gc) {
+		if(!exist)
+			return;
+		gc.strokeRect(x1, y1, x2-x1, y2-y1);
+	}
+	
+	public void drawCurrentOnCanvas(GraphicsContext gc) {
+		double newX1 = x1;
+		double newX2 = x2;
+		double newY1 = y1;
+		double newY2 = y2;
 		
+		if(x2 < x1) {
+			newX1 = x2;
+			newX2 = x1;
+		}
+		if(y2 < y1) {
+			newY1 = y2;
+			newY2 = y1;
+		}
+		gc.strokeRect(newX1, newY1, newX2-newX1, newY2-newY1);
+	}
+
+	public static List<Selection> getSelectionList() {
+		return selectionList;
+	}
+	
+	public void setFirstCorner(double x, double y) {
+		this.x1 = x;
+		this.y1 = y;
+	}
+	
+	public void setSecondCorner(double x, double y) {
+		exist = true;
+		this.x2 = x;
+		this.y2 = y;
+	}
+	
+	public static Selection getCurrentSelection() {
+		return currentSelection;
+	}
+	public void finalizeSelection() {
+		if(currentSelection != null) {
+			if(x2 < x1) {
+				double tmp = x1;
+				x1 = x2;
+				x2 = tmp;
+			}
+			if(y2 < y1) {
+				double tmp = y1;
+				y1 = y2;
+				y2 = tmp;
+			}
+			selectionList.add(currentSelection);
+			currentSelection = null;
+		}
+		deletedSelectionList.clear();
+	}
+
+	public static void initSelection(double x, double y) {
+		currentSelection = new Selection();
+		currentSelection.setFirstCorner(x, y);
 	}
 }
--- src/application/GUI/SelectionPanel.java
+++ src/application/GUI/SelectionPanel.java
@@ -1,13 +1,57 @@
 package application.GUI;
 
-import javafx.scene.Group;
+import application.Preferences;
+import application.EventHandlers.DrawSelectionListener;
 
-public class SelectionPanel extends Group {
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
+public class SelectionPanel extends Canvas {
 	
 	GUI gui;
+	double width, height;
+	GraphicsContext gc;
+	Color selectionColor;
 	
-	public SelectionPanel(GUI gui) {
+	double mouseX, mouseY;
+	
+	public SelectionPanel(GUI gui, double width, double height) {
 		this.gui = gui;
+		this.width = width;
+		this.height = height;
+		this.selectionColor = Preferences.getSelectionColor();
+		
+		setWidth(width);
+		setHeight(height);
+		gc = getGraphicsContext2D();
+		
+		addEventHandler(MouseEvent.MOUSE_PRESSED, new DrawSelectionListener(this));
+		addEventHandler(MouseEvent.MOUSE_DRAGGED, new DrawSelectionListener(this));
+        addEventHandler(MouseEvent.MOUSE_RELEASED, new DrawSelectionListener(this));
+		
+		draw();
+	}
+	
+	public void draw() {
+		gc.clearRect(0, 0, width, height);
+		gc.setStroke(selectionColor);
+		gc.setLineWidth(Preferences.getSelectionWidth());
+		for(Selection selection : Selection.getSelectionList()) {
+			selection.drawOnCanvas(gc);
+		}
+	}
+	
+	public void drawCurrentSelection() {
+		Selection.getCurrentSelection().drawCurrentOnCanvas(gc);
+	}
+	
+	public void resize(double width, double height) {
+		this.width = width;
+		this.height = height;
+		setWidth(width);
+		setHeight(height);
 	}
 
 }
