b9a34d3c59165b482a084a7979eff962a38d9e6a
xxlukasz11 <xxlukasz11@gmail.com>
2019-07-15 22:35:59 +0200

    export chart only while specific panels are loaded

--- src/EventHandlers/ExportChartHandler.java
+++ src/EventHandlers/ExportChartHandler.java
@@ -3,6 +3,7 @@ package EventHandlers;
 import DataModule.DcmData;
 import DataModule.DcmManager;
 import  GUI.GUI;
+import  GUI.DrawingPanel;
 import application.Preferences;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -11,12 +12,19 @@ public class ExportChartHandler implements EventHandler<ActionEvent> {
 
 	@Override
 	public void handle(ActionEvent event) {
-		if(DcmData.isDoseCalculated()) {
+		boolean correctPanelLoaded =
+				GUI.instance().getDrawingPanel().isCanvasEmbeded() ||
+				GUI.instance().getDrawingPanel().isChartEmbeded();
+
+		if(DcmData.isDoseCalculated() && correctPanelLoaded) {
 			int currentFrameNumber = GUI.instance().getDrawingPanel().getCurrentFrame();
 			DcmManager.exportChart(currentFrameNumber);
 		}
+		else if(correctPanelLoaded) {
+			GUI.instance().showInformationDialog(Preferences.getLabel("cannotExportChartTitle"), Preferences.getLabel("notCalculatedInformationContent"));
+		}
 		else {
-			GUI.instance().showInformationDialog(Preferences.getLabel("notCalculatedInformationTitle"), Preferences.getLabel("notCalculatedInformationContent"));
+			GUI.instance().showInformationDialog(Preferences.getLabel("cannotExportChartTitle"), Preferences.getLabel("cannotExportChartContent-wrongPanel"));
 		}
 	}
 
